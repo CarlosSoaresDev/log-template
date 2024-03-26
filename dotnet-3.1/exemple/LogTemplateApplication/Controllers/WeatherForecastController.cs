83e1f42b3c616bfae8ef5587389f5c4dd75edf1d
Carlos Soares <carlos.soares@crmall.com>
2024-03-25 23:12:35 -0300

    Publish log template 3.1 version

---
+++ dotnet-3.1/exemple/LogTemplateApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogTemplateApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.StartLogInformation(new Template
+            {
+                Title = "Started Process",
+                Description = "No details"
+            });
+
+            var rng = new Random();
+            var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            _logger.StartLogInformation(new Template
+            {
+                Title = "Finished Process",
+                Description = "No details"
+            });
+
+            return weatherForecasts;
+        }
+
+        [HttpGet("Error")]
+        public IEnumerable<WeatherForecast> GetWithError()
+        {
+            try
+            {
+                _logger.StartLogInformation(new Template
+                {
+                    Title = "Started Process",
+                    Description = "No details"
+                });
+
+                throw new ArgumentException("Intentional error");
+            }
+            catch (Exception ex)
+            {
+                _logger.EndLogError(new TemplateError
+                {
+                    Title = "Ended Process",
+                    Description = "No details",
+                    MessageErro = ex.Message,
+                    StackTrace = ex.StackTrace
+                });
+
+                throw;
+            }
+        }
+    }
+}
---
+++ dotnet-3.1/src/LogTemplate/Models/TemplateBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Logging
+{
+    public class TemplateBase
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string TemplateId = Guid.NewGuid().ToString();
+        public string Status { get; internal set; }
+        public string Application { get; internal set; }
+        public string? CorrelationId { get; internal set; }
+        public string LogLevel { get; internal set; }
+    }
+}
